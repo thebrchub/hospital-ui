deb861c44708c269a81d302ccef957c96e9cc00c
Srikanth <srikanthraj.brchubllp@gmail.com>
2025-05-23 19:04:02 +0530

    updated as required

---
+++ tailwind.config.js
@@ -0,0 +1,9 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{js,jsx}"],
+  theme: {
+    extend: {},
+  },
+  darkMode: "class", // enables dark mode via class
+  plugins: [],
+}
